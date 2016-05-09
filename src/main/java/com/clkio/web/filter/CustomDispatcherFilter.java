5d32412bb1f1b3ee8d083e59ce7ef5e12c7debf1
Jean Villete <jean.villete@gmail.com>
2016-05-09 17:59:17 -0300

    Adding CustomDispatcherFilter.java in order to turn possible to map urls closer to rest style, once it was not possible to be achieved through simple url-mappings.

---
+++ src/main/java/com/clkio/web/filter/CustomDispatcherFilter.java
@@ -0,0 +1,50 @@
+package com.clkio.web.filter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class CustomDispatcherFilter implements Filter {
+
+	private static final Map< String, String > mappings = new HashMap< String, String >();
+	
+	@Override
+	public void init( FilterConfig filterConfig ) throws ServletException {
+		String customDispatcherForward = filterConfig.getInitParameter( "custom-dispatcher-forward" );
+		
+		if ( customDispatcherForward == null || ( customDispatcherForward = customDispatcherForward.trim() ).isEmpty() )
+			throw new IllegalStateException( "No proper value provided for 'custom-dispatcher-forward'." );
+		
+		for ( String line : customDispatcherForward.split( "\\n" ) ) {
+			String[] elements = line.split( "=>" );
+			if ( elements.length != 2 ) throw new IllegalStateException( "Invalid pair value; line=[" + line + "]" );
+			mappings.put( elements[0].trim(), elements[1].trim() );
+		}
+	}
+
+	@Override
+	public void doFilter( ServletRequest request, ServletResponse response, FilterChain chain ) throws IOException, ServletException {
+		String url = ( ( HttpServletRequest ) request ).getRequestURL().toString();
+		for ( String pattern : mappings.keySet() )
+			if ( Pattern.compile( pattern ).matcher( url ).matches() ) {
+				request.getRequestDispatcher( url.replaceAll( pattern, mappings.get( pattern ) ) ).forward( request, response );
+				return;
+			}
+		chain.doFilter( request, response );
+	}
+
+	@Override
+	public void destroy() {
+		mappings.clear();
+	}
+
+}
